bbe4092cfe11d0b01a67f1331020ff71d3f8e0d9
luziana13 <luziana1310@gmail>
2020-06-01 13:32:04 -0300

    banco local

--- backend/database.js
+++ backend/database.js
@@ -3,10 +3,10 @@ const knex = require("knex");
 const database = knex({
     client: "pg", // pg is the database library for postgreSQL on knexjs
     connection: {
-        host: "ec2-54-81-37-115.compute-1.amazonaws.com", // Your local host IP
-        user: "hmwgbdqjflhaah", // Your postgres user name
-        password: "820840ef265b538fa9792b28af5fcb6cacddffdb5cdcc2cd29aec98cecf1993b", // Your postgres user password
-        database: "d5m7b5rk930bjm" // Your database name
+        host: "localhost", // Your local host IP
+        user: "postgres", // Your postgres user name
+        password: "postgres", // Your postgres user password
+        database: "FrontCovid" // Your database name
 
 
     }
