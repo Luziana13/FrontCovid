4e8c8433436ee1af3cc1c263f083a247576dcfd3
luziana13 <luziana1310@gmail>
2020-06-01 23:46:34 -0300

    testepg

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,13 @@
 const knex = require("knex");
+
 //DADOS DO BANCO QUE SERÁ UTILIZADO
 const database = knex({
     client: "pg", // pg is the database library for postgreSQL on knexjs
     connection: {
-        host: "localhost", // Your local host IP
-        user: "postgres", // Your postgres user name
-        password: "postgres", // Your postgres user password
-        database: "FrontCovid" // Your database name
+        host: process.env.DB_HOST, // Your local host IP
+        user: process.env.DB_USER, // Your postgres user name
+        password: process.env.DB_PASS, // Your postgres user password
+        database: process.env.DB_DATABASE// Your database name
 
 
     }
